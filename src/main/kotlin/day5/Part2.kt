031fb562639c50c96ce31f4c1e2eb4cbaf8ec087
Prelipcean Cristian <prelipceancristian100@gmail.com>
2023-12-06 00:18:16 +0200

    Day 5 part extended

--- src/main/kotlin/day5/Part2.kt
+++ src/main/kotlin/day5/Part2.kt
@@ -6,8 +6,8 @@ import common.readTestLines
 fun main() {
 //    val lines = readTestLines(5)
     val lines = readInputLines(5)
-    val seeds = readSeedsPart2(lines[0])
+//    val seeds = readSeedsPart2(lines[0])
     val mappings = buildMappings(lines)
-    val locations = mapSeedsToLocation(seeds, mappings)
-    println(locations.min())
+//    val locations = mapSeedsToLocation(seeds, mappings)
+//    println(locations.min())
 }
