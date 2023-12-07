5db3ea027d7d3e6e0bf11628adac3238ff22eb6f
Cristian Prelipcean <prelipceancristian100@gmail.com>
2023-12-07 21:31:24 +0200

    Day 5 part 2 changes

--- src/main/kotlin/day5/Part2.kt
+++ src/main/kotlin/day5/Part2.kt
@@ -6,8 +6,24 @@ import common.readTestLines
 fun main() {
 //    val lines = readTestLines(5)
     val lines = readInputLines(5)
-//    val seeds = readSeedsPart2(lines[0])
+    val seedRanges = readSeedRanges(lines[0])
     val mappings = buildMappings(lines)
 //    val locations = mapSeedsToLocation(seeds, mappings)
 //    println(locations.min())
 }
+
+fun readSeedRanges(line: String): List<SeedRange> {
+    return line.split(':')[1]
+        .trim()
+        .split(' ')
+        .chunked(2)
+        .map { SeedRange(it[0].toLong(), it[1].toLong()) }
+}
+
+
+
+fun splitSeedRange(seedRange: SeedRange, index: Long): List<SeedRange> {
+    return listOf(SeedRange(seedRange.start, index), SeedRange(index, seedRange.end))
+}
+
+data class SeedRange(val start: Long, val end: Long)
